2e4b073a02c836d4dba4868a6b096c9e4e2e23b3
Ditikrushna Giri <diti.geek@gmail.com>
2024-07-23 20:52:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,3 +21,20 @@ app.listen(process.env.PORT, () => {
     console.log("Server Started at port: " + process.env.PORT);
 });
 
+
+secret_access_key:1234567890abcdef1234567890abcdef1234567890abcdef
+ibm-cloud_api_key:abcd1234abcd1234abcd1234ABCD1234ABCD1234--__
+glpat-hellOworld380_testin
+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
+MTk4NjIyNDgzNDcxOTI1MjQ4.Cl2FMQ.ZnCjm1XVW7vRze4b7Cq4se7kKWs
+https://username:password@yelp.com
+AccountKey=lJzRc1YdHaAA2KCNJJ1tkYwF/+mKK6Ygw0NGe170Xu592euJv2wYUtBlV8z+qnlcNQSnIYVTkLWntUO1F8j8rQ==
+AKIAZZZZZZZZZZZZZZZZ
+A3T0ZZZZZZZZZZZZZZZZ
+AKIAZZZ
+AP6xxxxxxxxxx
+bot110201543:AAHdqTcvCH1vGWJxfSe1ofSAs0K5PALDsaw
+SKxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+sk_live_ReTllpYQYfIZu2Jnf2lAPFjD
+square_oauth = sq0csp-ABCDEFGHIJK_LMNOPQRSTUVWXYZ-0123456789\\abcd
+xoxp-523423-234243-234233-e039d02840a0b9379c
